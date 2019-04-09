b23be5088949f43b148b3443cbe00846bdd33e74
frauboes <juliaboes@posteo.de>
2019-04-09 14:28:41 +0100

    complete task1 project3

--- miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
+++ miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
@@ -1,6 +1,6 @@
 package edu.coursera.parallel;
 
-import static edu.rice.pcdp.PCDP.forseq2d;
+import static edu.rice.pcdp.PCDP.*;
 
 /**
  * Wrapper class for implementing matrix multiply efficiently in parallel.
@@ -44,7 +44,7 @@ public final class MatrixMultiply {
          * TODO Parallelize this outermost two-dimension sequential loop to
          * achieve performance improvement.
          */
-        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
+        forall2dChunked(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
                 C[i][j] += A[i][k] * B[k][j];
